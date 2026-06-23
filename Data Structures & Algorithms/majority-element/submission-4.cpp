6e74ebc5b9dea247a68cb0ef742ba00695298da2
Nishanth <knishanth797@gmail.com>
2026-06-23 11:49:47 +0530

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.cpp
@@ -0,0 +1,28 @@
+//mooore's voting algorithm
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int el = 0;
+        int cnt = 0;
+        for(int i=0;i<nums.size();i++){
+            if(cnt==0){
+                cnt=1;
+                el=nums[i];
+
+            }
+            else if(nums[i]==el){
+                cnt++;
+
+
+
+            }
+            else{
+                cnt--;
+            }
+
+        }
+        return el;
+        
+    }
+};

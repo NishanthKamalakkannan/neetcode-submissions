1c92ce25cd822452c4067303a4c250661fc22f64
Nishanth <knishanth797@gmail.com>
2026-06-24 12:42:22 +0530

    Add: rearrange-array-elements-by-sign - submission-0

---
+++ Data Structures & Algorithms/rearrange-array-elements-by-sign/submission-0.cpp
@@ -0,0 +1,31 @@
+//BRUTE FORCE SOLUTION
+
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+
+        vector<int> pos;
+        vector<int> neg;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            if(nums[i] > 0) {
+                pos.push_back(nums[i]);
+            }
+            else {
+                neg.push_back(nums[i]);
+            }
+        }
+
+        vector<int> ans(nums.size());
+
+        for(int i = 0; i < pos.size(); i++) {
+
+            ans[2*i] = pos[i];
+            ans[2*i + 1] = neg[i];
+        }
+
+        return ans;
+    }
+};

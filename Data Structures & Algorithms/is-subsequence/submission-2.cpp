c45621db52d61ead1364c73331b4a2b396bab65a
Nishanth <knishanth797@gmail.com>
2026-07-04 13:04:04 +0530

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int start=0;
+        for(int i=0;i<s.size();i++){
+            
+            bool found =false;
+            for(int j= start;j<t.size();j++){
+                if(s[i]==t[j]){
+                    found = true;
+                    start = j+1;
+                    break;
+
+                    
+                }
+            }
+            if(!found)
+                return false;
+            
+        }
+        return true;
+        
+    }
+};
